cac6a68e2bfae861b74dbafa89876c362c5d79e8
lmells98 <leightn.mells23@gmail.com>
2021-10-11 14:08:00 +1030

    Libft Header File

--- srcs/libft/includes/libft.h
+++ srcs/libft/includes/libft.h
@@ -1,9 +1,12 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
+# include <unistd.h>
 # include <stdlib.h>
 
-size_t	ft_strlen(const char *str);
 int		ft_strchr(const char *s, int c);
+void	ft_putstr(const char *str);
+size_t	ft_strlen(const char *str);
+
 
 #endif
