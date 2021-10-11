1b3f21ce5fa3d0347740ad5251e0f202c5c79426
lmells98 <leightn.mells23@gmail.com>
2021-10-11 14:08:40 +1030

    Print Number Type Conversions

--- srcs/print_nbr_types.c
+++ srcs/print_nbr_types.c
@@ -1,32 +1,23 @@
 #include "../includes/ft_printf.h"
 #include "./libft/includes/libft.h"
 
-#include <stdio.h>
-
-int	check_is_neg(long int nbr)
+static short	check_is_neg(long int nbr)
 {
 	if (nbr < 0)
 	{
-		printf("\nHELLO\n");
-		write(1, "-", 1);
 		return (1);
 	}
 	else
 		return (0);
 }
 
-static int	get_nbr_len(long int nbr)
+static int	get_nbr_len(unsigned long int nbr)
 {
 	int	len;
 
 	len = 0;
 	if (nbr == 0)
 		return (1);
-	if (nbr < 0)
-	{
-		nbr *= -1;
-		len += 1;
-	}
 	while (nbr != 0)
 	{
 		nbr /= 10;
@@ -35,49 +26,44 @@ static int	get_nbr_len(long int nbr)
 	return (len);
 }
 
-/*
- * void	dump_arr(void *array)
- * {
- * 		char			ch_p;
- * 		char			*temp;
- * 		unsigned int	i;
- *
- * 		if (!array)
- * 		return ;
- * 		temp = (char *)array;
- * 		for (i = 0; i < ft_strlen((char *)array); i++)
- * 		{
- * 			ch_p = temp[i];
- * 			printf("\nindex=%i\tchar=%c", i, ch_p);
- * 			}
- * 		printf("\n\n");
- * 	}
-*/
-
-int	li_itoa(long int nbr)
+int	print_digits(unsigned long int nbr, int sign)
 {
-	int	len;
-	int	ret;
+	int		ret;
+	int		len;
 	char	*values;
 
 	ret = 0;
 	len = get_nbr_len(nbr);
+	if (sign)
+		len += 1;
 	values = (char *)malloc((len + 1) * sizeof(char));
-	values[0] = '0';
-	values[len] = 0;
-	if (nbr < 0)
+	if (values)
 	{
-		values[0] = '-';
-		nbr *= -1;
+		values[0] = '0';
+		values[len] = 0;
+		if (sign == 1)
+			values[0] = '-';
+		while (nbr && len-- > 0)
+		{
+			values[len] = (nbr % 10) + '0';
+			nbr /= 10;
+		}
+		ret += print_str(values);
 	}
-	while (nbr && len-- > 0)
-	{
-		values[len] = (nbr % 10) + '0';
-		nbr /= 10;
-	}
-//	dump_arr(values);
-	ret += print_str(values);
 	free(values);
+	return (ret);
+}
+
+int	itoa(long int nbr)
+{
+	int		ret;
+	int		sign;
+
+	ret = 0;
+	sign = check_is_neg(nbr);
+	if (sign)
+		nbr *= -1;
+	ret += print_digits(nbr, sign);
 	return (ret);
 }
 
@@ -87,6 +73,8 @@ int	print_nbr_types(char type, va_list *arg_p)
 
 	ret = 0;
 	if (ft_strchr("di", type))
-		ret += li_itoa(va_arg(*arg_p, int));
+		ret += itoa(va_arg(*arg_p, int));
+	else if (type == 'u')
+		ret += print_digits(va_arg(*arg_p, unsigned int), 0);
 	return (ret);
 }
