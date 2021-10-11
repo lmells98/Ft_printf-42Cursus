a9c249354a5fbf4330c075a607e6ead90728cb0e
lmells98 <leighton.mells23@gmail.com>
2021-10-11 23:05:15 +1030

    Main File for Testing

--- main.c
+++ main.c
@@ -4,18 +4,18 @@
 
 int main(void)
 {
-	int ret;
-	int	test;
+	int 	ret;
+	int		nb;
 
 	ret = 0;
-	test = 0;
+	nb = 420;
 
 	printf("EO:\n[");
-	ret += printf("%d", test);
+	ret += printf("%x", nb);
 	printf("] chars = %i\n", ret);
 	printf("AO:\n");
 	write(1, "[", 1);
 	ret = 0;
-	ret += ft_printf("%d", test);
+	ret += ft_printf("%x", nb);
 	printf("] chars = %i\n", ret);
 }
