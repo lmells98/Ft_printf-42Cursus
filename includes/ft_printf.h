d0dc2876bc99e46e5e5ea3d1c2a20715a87aff66
lmells98 <leightn.mells23@gmail.com>
2021-10-11 14:07:03 +1030

    ft_printf.h | Header File

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -5,12 +5,12 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-int		ft_printf(const char *format, ...);
-int		verify_type(const char **format);
-int		convert_type(const char **format, char type, va_list *arg_p);
-int		print_nbr_types(char type, va_list *arg_p);
-int		print_char_types(char type, va_list *arg_p);
-int		print_str(char *str);
-int		print_percent(char type);
+int	ft_printf(const char *format, ...);
+int	verify_type(const char **format);
+int	convert_type(const char **format, char type, va_list *arg_p);
+int	print_nbr_types(char type, va_list *arg_p);
+int	print_char_types(char type, va_list *arg_p);
+int	print_str(char *str);
+int	print_percent(char type);
 
 #endif
