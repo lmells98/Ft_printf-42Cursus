d038880a559cc8d9bd0b5368687459eef33c0003
lmells98 <leightn.mells23@gmail.com>
2021-10-14 09:55:01 +1030

    Libft | Header File

--- srcs/libft/includes/libft.h
+++ srcs/libft/includes/libft.h
@@ -5,6 +5,8 @@
 # include <stdlib.h>
 
 int		ft_strchr(const char *s, int c);
+char	*ft_itoa(long int nbr);
+char	*ft_ul_itoa(unsigned long int nbr, size_t size);
 void	ft_putstr(const char *str);
 size_t	ft_strlen(const char *str);
 
