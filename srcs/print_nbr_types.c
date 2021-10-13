92b04636b4013c57a08eaa0037e27fbf76410420
lmells98 <leightn.mells23@gmail.com>
2021-10-14 09:55:35 +1030

    Print Number Type Conversions

--- srcs/print_nbr_types.c
+++ srcs/print_nbr_types.c
@@ -1,62 +1,23 @@
 #include "../includes/ft_printf.h"
 #include "./libft/includes/libft.h"
 
-static int	get_nbr_len(unsigned long int nbr)
+static int	print_number(unsigned long int nbr)
 {
-	int	len;
-
-	len = 0;
-	if (nbr == 0)
-		return (1);
-	while (nbr != 0)
-	{
-		nbr /= 10;
-		len += 1;
-	}
-	return (len);
-}
-
-int	print_digits(unsigned long int nbr, int sign)
-{
-	int		ret;
-	int		len;
-	char	*values;
+	int	ret;
+	char *values;
 
 	ret = 0;
-	len = get_nbr_len(nbr);
-	if (sign)
-		len += 1;
-	values = (char *)malloc((len + 1) * sizeof(char));
-	if (values)
+	values = 0;
+	if (nbr != 0)
 	{
-		values[0] = '0';
-		values[len] = 0;
-		if (sign == 1)
-			values[0] = '-';
-		while (nbr && len-- > 0)
-		{
-			values[len] = (nbr % 10) + '0';
-			nbr /= 10;
-		}
+		values = ft_itoa(nbr);
+		if (!values)
+			return (ret);
 		ret += print_str(values);
+		free(values);
 	}
-	free(values);
-	return (ret);
-}
-
-int	itoa(long int nbr)
-{
-	int		ret;
-	int		sign;
-
-	ret = 0;
-	sign = 0;
-	if (nbr < 0)
-	{	
-		sign = 1;
-		nbr *= -1;
-	}
-	ret += print_digits(nbr, sign);
+	else
+		ret += print_char('0');
 	return (ret);
 }
 
@@ -66,8 +27,8 @@ int	print_nbr_types(char type, va_list *arg_p)
 
 	ret = 0;
 	if (ft_strchr("di", type))
-		ret += itoa(va_arg(*arg_p, int));
+		ret += print_number(va_arg(*arg_p, int));
 	else if (type == 'u')
-		ret += print_digits(va_arg(*arg_p, unsigned int), 0);
+		ret += print_number(va_arg(*arg_p, unsigned int));
 	return (ret);
 }
